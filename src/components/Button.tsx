afb29c3de1d981ec8ca2d452925d79ca2be1632c
rilton bispo <riltondev@gmail.com>
2023-08-23 16:56:11 -0300

    feat: create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type Props = {
+  icon: ReactNode;
+  onClick?: () => void
+};
+
+const Button = ({...props}: Props) => {
+  return (
+    <div className="w-10 h-10 rounded-3xl flex items-center justify-center" onClick={props.onClick}>{props.icon}</div>
+  );
+};
+
+export default Button;
