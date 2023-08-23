a3e10f2a90b9517c569761ce150967059bc061ee
rilton bispo <riltondev@gmail.com>
2023-08-23 16:55:59 -0300

    feat: add types

---
+++ src/types/ChatTypes.ts
@@ -0,0 +1,16 @@
+export type ChatType = {
+  chatId: number;
+  title: string
+  image: string
+};
+
+export type MessageType = {
+  author: number
+  body: string
+}
+
+export type UserType = {
+  id: number
+  avatar: string
+  name: string
+}
