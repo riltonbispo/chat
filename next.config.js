82d0eeab6f7b5602c9221f68ed10d74a124e961f
rilton bispo <riltondev@gmail.com>
2023-08-23 16:55:22 -0300

    firts commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["w7.pngwing.com", "blog.iprocess.com.br"],
+    formats: ["image/avif", "image/webp"],
+  },
+};
