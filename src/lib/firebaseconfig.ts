293c245a2fe61cbb0ce3094687621bf6b16620aa
rilton bispo <riltondev@gmail.com>
2023-08-28 15:17:02 -0300

    feat: create chat

--- src/lib/firebaseconfig.ts
+++ src/lib/firebaseconfig.ts
@@ -1,7 +1,13 @@
-'use client'
+"use client";
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { getDocs, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import {
+  getDocs,
+  getFirestore,
+  doc,
+  updateDoc,
+  arrayUnion,
+} from "firebase/firestore";
 import { addDoc, collection } from "firebase/firestore";
 
 import { UserType } from "@/types/ChatTypes";
@@ -12,32 +18,35 @@ const firebaseConfig = {
   projectId: "chat-3cc67",
   storageBucket: "chat-3cc67.appspot.com",
   messagingSenderId: "381169874978",
-  appId: "1:381169874978:web:4bb3c8ea77b9702356f27b"
+  appId: "1:381169874978:web:4bb3c8ea77b9702356f27b",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-const useCollections = collection(db, 'users')
+const usersCollections = collection(db, "users");
+const chatsCollections = collection(db, "chats");
 
 export const addUser = (email: string, name: string) => {
-  addDoc(useCollections, { name, email })
-}
+  addDoc(usersCollections, { name, email });
+};
 
 export const currentUser = async (email: string) => {
   try {
-    const result = await getDocs(useCollections);
-    const foundUser = result.docs.find(doc => doc.data().email === email);
-    
+    const result = await getDocs(usersCollections);
+    const foundUser = result.docs.find((doc) => doc.data().email === email);
+
     if (foundUser) {
       const data = foundUser.data();
-      console.log(`id: ${foundUser.id} username: ${data.name} email: ${data.email}`)
+      console.log(
+        `id: ${foundUser.id} username: ${data.name} email: ${data.email}`
+      );
       return {
         id: foundUser.id,
         username: data.name,
-        email: data.email
+        email: data.email,
       };
     } else {
       return null;
@@ -48,20 +57,47 @@ export const currentUser = async (email: string) => {
   }
 };
 
-export const contactList = async (userId: string) =>{
-  let list: UserType[] = []
-  let result = await getDocs(useCollections)
-  result.forEach(result => {
-    let data = result.data()
+export const contactList = async (userId: string) => {
+  let list: UserType[] = [];
+  let results = await getDocs(usersCollections);
+  results.forEach((result) => {
+    let data = result.data();
 
-    if(result.id != userId){
+    if (result.id != userId) {
       list.push({
         id: result.id,
         username: data.name,
-        email: data.email
-      })
+        email: data.email,
+      });
     }
-  })
-  
-  return list
-}
+  });
+
+  return list;
+};
+
+export const createChat = async (u1: UserType, u2: UserType) => {
+  let newChat = await addDoc(chatsCollections, {
+    messages: [],
+    users: [u1.id, u2.id],
+  });
+
+  const user = doc(db, "users", u1.id);
+  const user2 = doc(db, "users", u2.id);
+
+  await updateDoc(user, {
+    chats: arrayUnion({
+      chatId: newChat.id,
+      title: u2.username,
+      with: u2.id,
+    }),
+  });
+
+  await updateDoc(user2, {
+    chats: arrayUnion({
+      chatId: newChat.id,
+      title: u1.username,
+      with: u1.id,
+    }),
+  });
+
+};
