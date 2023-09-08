1c7916349d6c1022cf21496673f71611cf5cde20
rilton bispo <riltondev@gmail.com>
2023-09-08 17:12:31 -0300

    style: newchat

--- src/components/NewChat.tsx
+++ src/components/NewChat.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import Button from "@/components/Button";
 import { useEffect, useState } from "react";
-import { RiArrowLeftLine } from "react-icons/ri";
 import { Chat, User } from "@/types/allTypes";
 import {
   contactListAction,
@@ -40,21 +38,16 @@ const NewChat = ({ ...props }: Props) => {
   };
 
   return (
-    <div
-      className={`w-1/3 max-w-md fixed top-0 bottom-0 bg-white flex flex-col transition-transform duration-300 transform ${
-        props.show ? "left-0" : "-translate-x-full"
-      }`}
-    >
-      <div className="flex bg-indigo-600 items-center p-4 text-white">
+    <div className={`w-56 max-w-md bg-orange-400 flex flex-col }`}>
+      <div className="flex bg-stone-900 items-center p-4 text-white">
         <div className="flex-1 text-center text-lg">Nova Conversa</div>
-        <Button icon={<RiArrowLeftLine />} onClick={handleClose} />
       </div>
-      <div className="flex-1 overflow-y-auto scroll">
+      <div className="flex-1 overflow-y-auto scroll__newchat">
         {list.map((item, key) => (
           <div
             onClick={() => addNewChat(item)}
             key={key}
-            className="p-4 cursor-pointer hover:bg-indigo-100 transition-colors duration-300"
+            className="p-4 cursor-pointer hover:bg-stone-800 hover:text-white transition-colors duration-300 text-stone-900"
           >
             <p className="text-lg">{item.name}</p>
           </div>
